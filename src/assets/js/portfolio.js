e81b4b199b60292863b0cfe39b65e263fd3d67a1
erica <powerwmj@gmail.com>
2024-05-23 15:38:31 +0900

    [작업중] 포트폴리오 list 디자인 수정

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
     renderHtml(data) {
       return `<li class="list__item">
         <a href="./detail/?id=${data.id}" class="list__link">
-          <div class="img-box">
+          <div class="img-box list__img">
             <img src="${data.imgUrl.pc}" alt="" class="m-hide">
             <img src="${data.imgUrl.mo}" alt="" class="m-show">
           </div>
