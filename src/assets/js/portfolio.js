2bd5c862da29b94501ba6a0fd21dcac74540eee9
erica <powerwmj@gmail.com>
2024-05-24 10:25:38 +0900

    [ing] 포트폴리오 list img-box 추가

--- src/assets/js/portfolio.js
+++ src/assets/js/portfolio.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
     renderHtml(data) {
       return `<li class="list__item">
         <a href="./detail/?id=${data.id}" class="list__link" data-cursor-text="View">
-          <div class="list__img">
+          <div class="img-box list__img">
             <img src="${data.imgUrl.pc}" alt="">
           </div>
           <div class="list__text">
