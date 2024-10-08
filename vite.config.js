7c13cbfcb6759c5b10edd3657c43f233fdc199d2
leejuhee <jhlee@bstones.co.kr>
2024-10-08 17:37:44 +0900

    js에서 라이브, 테스트 서버 분류

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig(() => {
   // 테스트 서버
-  const isTest = false;
+  const isTest = true;
   // 라이브 서버
   // const isTest = false;
   const baseRoot = isTest ? '/bstones/' : '/';
